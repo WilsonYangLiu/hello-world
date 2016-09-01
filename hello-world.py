f9d9dcb38ead9b8e1f046b8c5ab5b0b080ad3fb6
WilsonYangLiu <lweixin316@gmail.com>
2016-09-01 13:44:49 +0800

    Add hello world script by python

---
+++ hello-world.py
@@ -0,0 +1,18 @@
+﻿#!/etc/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+class hello(object):
+	
+	def __init__(self, str='world'):
+		self._name = str
+		
+	def __str__(self):
+		return 'hello {}'.format(self._name)
+		
+	__repr__ = __str__
+	
+if __name__ == '__main__':
+	
+	print hello()
+	print hello('friend!')
